f2ece317ecb922776e86d5829cb1bffc8dd37d28
Mika <m.tores63@outlook.fr>
2025-12-12 13:53:23 +0100

    COMPLETED: exercice_pdo__1 exercice 1

--- Exercice-PDO-1/Exercice_1/index.php
+++ Exercice-PDO-1/Exercice_1/index.php
@@ -1,5 +1,14 @@
 <?php
     require_once "../bdd_connect.php";
+
+    $request = $db->query("SELECT 
+                                lastname,
+                                firstname
+                            FROM
+                                clients"
+                        );
+    
+    $users = $request->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +19,15 @@
     <title>Exercice_1</title>
 </head>
 <body>
-    
+   <ul>
+    <h1>Voici la liste des clients :</h1>
+    <?php
+        foreach ($users as $user) {
+    ?>
+        <li><?= $user['lastname'] . " " . $user['firstname'];?></li>
+    <?php
+        }
+    ?>
+   </ul>
 </body>
 </html>
