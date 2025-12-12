57dc1825191828f8b2348fd2e666aff63a53bef6
Mika <m.tores63@outlook.fr>
2025-12-12 16:23:33 +0100

    COMPLETED: exercice_pdo__1

--- Exercice-PDO-1/Exercice_7/index.php
+++ Exercice-PDO-1/Exercice_7/index.php
@@ -1,15 +1,57 @@
 <?php
-    require_once "../bdd_connect.php";
+require_once "../bdd_connect.php";
+
+$request = $db->query(
+    "SELECT
+	    lastName,
+        firstName,
+        birthDate,
+        card,
+        cardNumber
+    FROM
+	    clients;"
+);
+
+$customers = $request->fetchAll();
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice_1</title>
+    <title>Exercice_7</title>
 </head>
+
 <body>
-    
+    <?php
+    foreach ($customers as $customer) {
+        $birthDate = new DateTime($customer['birthDate']);
+        $formatBirthDate = $birthDate->format('d/m/Y');
+    ?>
+        <div>
+            <p><?= "Nom : " . $customer['lastName'] ?></p>
+            <p><?= "Prénom : " . $customer['firstName'] ?></p>
+            <p><?= "Date de naissance : " . $formatBirthDate ?></p>
+            
+            <?php
+            if ($customer['card'] === 1) {
+            ?>
+                <p><?= "Carte de fidélité : oui " ?></p>
+                <p><?= "Numéro de carte : " . $customer['cardNumber'] ?></p>
+            <?php
+            } else {
+            ?>
+                <p><?= "Carte de fidélité : non" ?></p>
+            <?php
+            };
+            ?>
+        </div><br>
+    <?php
+    }
+    ?>
 </body>
+
 </html>
