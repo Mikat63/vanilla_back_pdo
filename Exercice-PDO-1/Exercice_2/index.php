9a162bba5fa24d88186dc5a2a516982c76a66794
Mika <m.tores63@outlook.fr>
2025-12-12 14:05:08 +0100

    COMPLETED: exercice_pdo__1 exercice 2

--- Exercice-PDO-1/Exercice_2/index.php
+++ Exercice-PDO-1/Exercice_2/index.php
@@ -1,15 +1,31 @@
 <?php
-    require_once "../bdd_connect.php";
+require_once "../bdd_connect.php";
+
+$request = $db->query("SELECT * FROM showtypes");
+
+$showTypes = $request->fetchAll();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercice_1</title>
+    <title>Exercice_2</title>
 </head>
+
 <body>
-    
+    <h1>Voici les différents types de spéctacles :</h1>
+    <ul>
+    <?php
+    foreach ($showTypes as $showType) {
+    ?>
+        <li><?= $showType['type']?></li>
+    <?php  
+    }
+    ?>
+    </ul>
 </body>
+
 </html>
